150e42eab9753e95a22384c237f29194609d3e13
ImeonGato <54463644+ImeonGato@users.noreply.github.com>
2020-01-26 20:16:25 -0700

    Addning back end files to site

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Kangooroo!</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper indexPage">                                                    <div class="mainSection">
+
+			<div class="logoContainer">
+					<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google icon image">
+				</div>
+				
+				<div class="searchContainer">
+
+					<form action="search.php" method="GET">
+
+						<input class="searchBox" type="text" name="term">
+						<input class="searchButton" type="submit" value="search">
+						
+					</form>
+
+				</div>
+			</div>
+
+		</div>
+</body>
+</html>
