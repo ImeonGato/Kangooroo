989240c3d1d87ce3160b5a54653a95f33ca77771
ImeonGato <54463644+ImeonGato@users.noreply.github.com>
2020-02-04 21:57:19 -0700

    search.php
    Added image and added class of search button.

--- search.php
+++ search.php
@@ -1,5 +1,65 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Kangooroo!</title>
 
-echo $_GET["term"];
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
-?>
+</head>
+<body>
+
+		<div class="wrapper">  
+
+			<div class="header">
+
+				<div class="headerContent">
+					
+						
+						<div class="logoContainer">			
+							<a href="index.php">
+								<img src="assets/images/kangooroo.png">
+							</a>
+						</div>
+
+
+						<div class="searchContainer">
+
+							<form action="search.php" method="GET">
+
+								<div class="searchBarContainer">
+
+									<input class="searchBox" type=text" name="term">
+									<button class="searchButton">
+										<img src="assets/images/icons/search.png">
+									</button>
+									
+									
+
+								</div>
+								
+
+							</form>
+							
+
+						</div>
+
+
+
+				</div>
+			
+				<div class="tabsContainer">
+					
+					<ul class="tabsList">
+
+						<li>Sites</li>
+						<li>Images</li>
+
+
+				</div>
+				
+			</div>
+
+	</div>
+
+</body>
+</html>
