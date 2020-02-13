edacbb75f890fd774570ddda7e15292f6a35046b
ImeonGato <54463644+ImeonGato@users.noreply.github.com>
2020-02-12 19:08:01 -0700

    Removing relative links

--- crawl.php
+++ crawl.php
@@ -2,7 +2,27 @@
 include("classes/DomDocumentParser.php");
 
 function createLink($src, $url) {
-	
+    
+    $scheme = parse_url($url) ["scheme"]; // http
+    $host = parse_url($url)["host"]; // www.reecekenney.com 
+
+    if(substr($src, 0, 2) == "//") {
+    		$src = $scheme . ":" . $src; 
+    }
+    else if(substr($src, 0, 1) == "/") {
+    	$src = $scheme . "://" . $host . $src; 
+    }
+    else if(substr($src, 0, 2) == "./") {
+    	$src = $scheme . "://" . $host . dirname(parse_url)($url)["path"] . substr($src, 1); 
+    }
+    else if(substr($src, 0, 3) == "../") {
+    	$src = $scheme . "://" . $host . "/" . $src; 
+	}
+	else if(substr($src, 0, 5) !== "https" && substr($src, 0, 4) !== "http") {
+		$src = $scheme . "://" . $host . "/" . $src; 
+	}
+
+	return $src; 
 }
 
 function followLinks($url) {
@@ -21,16 +41,17 @@ function followLinks($url) {
 			continue; 
 		}
 
-		createLink($href, $url);
+	
+			
+		$href == createLink($href, $url);
 
-		//echo $href . "<br>";
+		echo  $href . "<br>";
 		
-
-		echo $href . "<br>";
 	}
+	
 	
 }
 
-$startUrl = "http://www.bbc.com";
+$startUrl = "http://www.foodnetwork.com";
 followLinks($startUrl); 
 ?>
