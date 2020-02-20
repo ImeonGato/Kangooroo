9d301f5b5d6862a265f32f787a740e1427d9c28b
ImeonGato <54463644+ImeonGato@users.noreply.github.com>
2020-02-19 21:44:16 -0700

    upload changes to crawl.php

--- crawl.php
+++ crawl.php
@@ -1,35 +1,41 @@
-<?php 
+<?php
 include("classes/DomDocumentParser.php");
 
+$alreadyCrawled = array();
+$crawling = array();
+
 function createLink($src, $url) {
-    
-    $scheme = parse_url($url) ["scheme"]; // http
-    $host = parse_url($url)["host"]; // www.reecekenney.com 
 
-    if(substr($src, 0, 2) == "//") {
-    		$src = $scheme . ":" . $src; 
-    }
-    else if(substr($src, 0, 1) == "/") {
-    	$src = $scheme . "://" . $host . $src; 
-    }
-    else if(substr($src, 0, 2) == "./") {
-    	$src = $scheme . "://" . $host . dirname(parse_url)($url)["path"] . substr($src, 1); 
-    }
-    else if(substr($src, 0, 3) == "../") {
-    	$src = $scheme . "://" . $host . "/" . $src; 
+	$scheme = parse_url($url)["scheme"]; // http
+	$host = parse_url($url)["host"]; // www.reecekenney.com
+	
+	if(substr($src, 0, 2) == "//") {
+		$src =  $scheme . ":" . $src;
 	}
-	else if(substr($src, 0, 5) !== "https" && substr($src, 0, 4) !== "http") {
-		$src = $scheme . "://" . $host . "/" . $src; 
+	else if(substr($src, 0, 1) == "/") {
+		$src = $scheme . "://" . $host . $src;
+	}
+	else if(substr($src, 0, 2) == "./") {
+		$src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+	}
+	else if(substr($src, 0, 3) == "../") {
+		$src = $scheme . "://" . $host . "/" . $src;
+	}
+	else if(substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+		$src = $scheme . "://" . $host . "/" . $src;
 	}
 
-	return $src; 
+	return $src;
 }
 
 function followLinks($url) {
 
-	$parser = new DomDocumentParser($url); 
+	global $alreadyCrawled;
+	global $crawling;
 
-	$linkList = $parser->getLinks(); 
+	$parser = new DomDocumentParser($url);
+
+	$linkList = $parser->getLinks();
 
 	foreach($linkList as $link) {
 		$href = $link->getAttribute("href");
@@ -38,20 +44,32 @@ function followLinks($url) {
 			continue;
 		}
 		else if(substr($href, 0, 11) == "javascript:") {
-			continue; 
+			continue;
 		}
 
-	
-			
-		$href == createLink($href, $url);
 
-		echo  $href . "<br>";
-		
+		$href = createLink($href, $url);
+
+
+		if(!in_array($href, $alreadyCrawled)) {
+			$alreadyCrawled[] = $href;
+			$crawling[] = $href;
+
+			// Insert $href
+		}
+
+
+		echo $href . "<br>";
 	}
-	
-	
+
+	array_shift($crawling);
+
+	foreach($crawling as $site) {
+		followLinks($site);
+	}
+
 }
 
 $startUrl = "http://www.foodnetwork.com";
-followLinks($startUrl); 
+followLinks($startUrl);
 ?>
